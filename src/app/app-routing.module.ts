eba4b8b208c88e896cbec00d4412ba9318a91f57
Elian027 <elian.moreira@epn.edu.ec>
2024-01-04 18:19:46 -0500

    Splash Screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { SplashComponent } from './splash/splash.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: SplashComponent
+  },
+  {
+    path: '', 
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
 ];
+
 
 @NgModule({
   imports: [
